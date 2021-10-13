26250fe20ec87184388969d9167946669be5e374
root <aqil@intermediatech.id>
2021-10-13 15:13:15 +0700

    perbaikan

--- resources/views/user/page/DataKegiatan.blade.php
+++ resources/views/user/page/DataKegiatan.blade.php
@@ -18,19 +18,14 @@
           <div class="service-item wow bounceInUp" data-wow-duration="1s" data-wow-delay="0.3s">
             <div class="row">
               <div class="col-lg-4">
-                 <a href="{{url('info/kegiatan/detail/'.$d->id_kegiatan)}}">
                 <div class="icon">
-                  <img src="{{$d->gambar}}" alt="">
+                  <a href="{{url('info/kegiatan/detail/'.$d->id_kegiatan)}}"><img src="{{$d->gambar}}" alt=""></a>
                 </div>
-              </a>
               </div>
               <div class="col-lg-8">
-                <a href="{{url('info/kegiatan/detail/'.$d->id_kegiatan)}}">
                 <div class="right-content">
-                  <h4>{{$d->nama_kegiatan}}</h4>
-                  <?php echo mb_strimwidth($d->keterangan, 0, 100, "..")?>
+                 <a href="{{url('info/kegiatan/detail/'.$d->id_kegiatan)}}"> <h4>{{$d->nama_kegiatan}}</h4></a>
                 </div>
-                </a>
               </div>
             </div>
           </div>
