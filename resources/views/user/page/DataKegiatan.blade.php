15d940280c92e4f8fdc9819f1ef4077d690111a4
root <aqil@intermediatech.id>
2021-10-13 15:06:11 +0700

    perbaikan

--- resources/views/user/page/DataKegiatan.blade.php
+++ resources/views/user/page/DataKegiatan.blade.php
@@ -14,25 +14,27 @@
     <div class="container-fluid">
       <div class="row">
         @foreach($data as $d)
-        <a href="{{url('info/kegiatan/detail/'.$d->id_kegiatan)}}">
         <div class="col-lg-4">
           <div class="service-item wow bounceInUp" data-wow-duration="1s" data-wow-delay="0.3s">
             <div class="row">
               <div class="col-lg-4">
+                 <a href="{{url('info/kegiatan/detail/'.$d->id_kegiatan)}}">
                 <div class="icon">
                   <img src="{{$d->gambar}}" alt="">
                 </div>
+              </a>
               </div>
               <div class="col-lg-8">
+                <a href="{{url('info/kegiatan/detail/'.$d->id_kegiatan)}}">
                 <div class="right-content">
                   <h4>{{$d->nama_kegiatan}}</h4>
-                  <?php echo mb_strimwidth($d->keterangan, 0, 184, "..")?>
+                  <?php echo mb_strimwidth($d->keterangan, 0, 100, "..")?>
                 </div>
+                </a>
               </div>
             </div>
           </div>
         </div>
-        </a>
         @endforeach
       </div>
     </div>
