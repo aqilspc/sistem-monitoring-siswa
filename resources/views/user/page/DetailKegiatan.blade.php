70a6743af278beb27457db71237d9149a3e0539c
root <aqil@intermediatech.id>
2021-12-27 19:21:07 +0700

    gambar

--- resources/views/user/page/DetailKegiatan.blade.php
+++ resources/views/user/page/DetailKegiatan.blade.php
@@ -6,8 +6,8 @@
       <div class="row">
         <div class="col-lg-6">
           <div class="left-image wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0.5s">
-            {{-- <img src="{{$data->gambar}}" alt=""> --}}
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/gSFU6fQKQsk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <img src="{{$data->gambar}}" alt=""> 
+          <!--   <iframe width="560" height="315" src="https://www.youtube.com/embed/gSFU6fQKQsk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
           </div>
         </div>
         <div class="col-lg-6 align-self-center wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s">
