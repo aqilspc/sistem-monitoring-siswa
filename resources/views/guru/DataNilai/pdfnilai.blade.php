15b513dcd9b2ae82bfcde467a7d67a58efd42fd0
naily26 <nailymasruri@gmail.com>
2021-12-27 10:51:32 +0700

    File pdf nilai

---
+++ resources/views/guru/DataNilai/pdfnilai.blade.php
@@ -0,0 +1,67 @@
+<html>
+    <body>
+    <div class="row">
+        <div class="col-lg-12margin-tb">
+          <div class="pull-leftmt-2">
+            <h3 style="text-align: center">LEMBAGA PENDIDIKAN</h3>
+            <h2 style="text-align: center">MIS NU SABILUN NAJAH 26</h2>
+            <h4 style="text-align: center">TAHUN AJARAN 2020/2021</h4><hr>
+          </div>
+          <p align="center">LAPORAN PENILAIN HASIL BELAJAR</p>
+          
+      </div>
+      <div>
+        <table style="width: 50%">
+          <tr>
+            <td>Nama</td><td>: Oh Sehun</td>
+          </tr>
+          <tr>
+            <td>NIS</td><td>: 00092131243</td>
+          </tr>
+          <tr>
+            <td>Kelas</td><td>: 5A</td>
+          </tr>
+        </table>
+      </div>
+      </div>
+      <table class="table table-hover" border="1" style="width: 100%"> 
+        <tbody>
+      <tr>
+        <th>No</th>
+        <th>Mata Pelajaran</th>
+        <th>KKM</th>
+        <th>Nilai</th>
+      </tr>
+      
+      
+      <tr>
+          <td align="center">01</td>
+          <td>Ilmu Pengetahuan Alam</td>
+          <td align="center">77</td>
+          <td align="center">90</td>
+          
+      </tr>
+      <tr>
+        <td align="center">02</td>
+        <td>Ilmu Pengetahuan Sosial</td>
+        <td align="center">77</td>
+        <td align="center">90</td>
+        
+    </tr>
+        </tbody>
+        <tfoot>
+            <tr>
+                <td colspan="3" align="center">Nilai rata-rata</td>
+                <td colspan="1" align="center">80</td>
+            </tr>
+
+        </tfoot>
+      
+    
+    
+      @endforeach
+      </table>
+      <!-- Bootstrap 4 -->
+      <script src="{{url('plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    </body>
+</html>
